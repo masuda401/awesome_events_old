29a67b07b8d529ca8571ebb32e9a9fa942f0d0f7
shinichi.masuda_823258 <shinichi.masuda_823258@glv.co.jp>
2024-01-13 21:54:51 +0900

    feat: 6-8-1 退会用コントローラ、ビュー、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resource :retirements
 end
