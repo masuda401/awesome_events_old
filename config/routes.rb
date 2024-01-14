7e8c3f2e5b347c73891527ed380a62f0314a2531
s-masuda <shinichi.masuda@gmail.com>
2024-01-14 15:03:49 +0900

    feat: 7-5-2 起動確認APIのテスト

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  get 'status' => 'status#index', defaults: { format: 'json' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resource :retirements
