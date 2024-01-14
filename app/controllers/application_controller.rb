e34d49f0ac9f3ae03838bb61cb12decc1293a89a
s-masuda <shinichi.masuda@gmail.com>
2024-01-14 22:03:26 +0900

    feat: 9-1-4 イベント告知アプリケーションのCIをGitHub Actionsで実行する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
   helper_method :logged_in?, :current_user
 
-  rescue_from Exception, with: :error500
-  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+  # rescue_from Exception, with: :error500
+  # rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
 
   def error404(e)
     render "error404", status: 404, formats: [:html]
