eb7b518e8fcb8eaf4d8cfe50da8cffa97724e898
s-masuda <shinichi.masuda@gmail.com>
2024-01-14 16:52:27 +0900

    feat: 8-3-1 エラーハンドリング

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resource :retirements
+
+  match "*path" => "application#error404", via: :all
 end
